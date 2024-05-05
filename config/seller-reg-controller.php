f6556af683a176c73e9f67b5f997ee52b4c4bf5c
Gaveen Sooriyaarachchi <149564638+kidhasia@users.noreply.github.com>
2024-05-05 12:25:19 +0530

    Update seller-reg-controller.php

--- config/seller-reg-controller.php
+++ config/seller-reg-controller.php
@@ -9,7 +9,10 @@ $reg_email = $_POST["eMail"];
 $reg_password = $_POST["passWord"];
 $file_name = $reg_firstname.$reg_lastname;
 
-echo $reg_firstname."<br>".$reg_lastname."<br>".$reg_phone."<br>".$reg_email;
+if ($reg_password !== $confirm_password) {
+    echo "<script>alert('Passwords do not match'); window.history.back();</script>";
+    exit(); 
+}
 
 $sql1="SELECT user_mail FROM user WHERE user_mail='$reg_email' ";
 $result=$conn->query($sql1);
@@ -19,12 +22,19 @@ if($result->num_rows>0)
     echo "<script> alert('This mail is already used')</script>";
 }
 else{
-    $sql= "INSERT INTO user (user_first_name, user_last_name,user_mail, user_password, profile_picture) VALUES ('$reg_firstname ','$reg_lastname ','$reg_email','$reg_password','$file_name')";
-    $result = $conn -> query($sql);
+    $sql= "INSERT INTO user (user_first_name, user_last_name,user_mail, user_password, profile_picture ) VALUES ('$reg_firstname ','$reg_lastname ','$reg_email','$reg_password','$file_name')";
 
-    if($result){
-        echo "Successfull"; 
+
+$result = $conn -> query($sql);
+if($conn->query($sql))
+{
+    echo "<script> alert('Insert Successful!')</script>";
+    // header('location:#');//Re-direct to home
+}
+else{
+        echo "Error".$conn->error;
     }
+    
 }
 
 
