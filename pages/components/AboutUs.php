c82040d357b1566fd2fbf7253a885026ce357435
Gaveen Sooriyaarachchi <149564638+kidhasia@users.noreply.github.com>
2024-05-07 00:53:51 +0530

    Update AboutUs.php

--- pages/components/AboutUs.php
+++ pages/components/AboutUs.php
@@ -4,7 +4,7 @@
 <link rel="stylesheet" href="../../styles/styles.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src= "../../script/AboutUs.js"></script>
+    <script src= "../../script/AboutUs.js" ></script>
     <title>Contact Us</title>
 </head>
 <body>
