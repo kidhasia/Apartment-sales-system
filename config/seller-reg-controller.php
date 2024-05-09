35bc3c3e510051c2ea3158253c2b6f4c8e34058c
Gaveen Sooriyaarachchi <149564638+kidhasia@users.noreply.github.com>
2024-05-09 22:34:24 +0530

    Update seller-reg-controller.php

--- config/seller-reg-controller.php
+++ config/seller-reg-controller.php
@@ -1,50 +1,33 @@
 <?php
 
-require_once '../config/db.php'; 
+require_once '../config/db.php';
 
 $reg_firstname = $_POST["first-name"];
 $reg_lastname = $_POST["last-name"];
-$reg_phone = $_POST["phoneNo"];
 $reg_email = $_POST["eMail"];
 $reg_password = $_POST["passWord"];
-$file_name = $reg_firstname.$reg_lastname;
+$file_name = $reg_firstname . $reg_lastname;
 
-$sql1="SELECT user_mail FROM user WHERE user_mail='$reg_email' ";
-$result=$conn->query($sql1);
+$sql1 = "SELECT user_mail FROM user WHERE user_mail = '$reg_email'";
+$result = $conn->query($sql1);
 
-if($result->num_rows>0)
-{
-    echo "<script> alert('This mail is already used')</script>";
-}
-else{
-    $sql= "INSERT INTO user (user_first_name, user_last_name,user_mail, user_password, profile_picture ) VALUES ('$reg_firstname ','$reg_lastname ','$reg_email','$reg_password','$file_name')";
+if ($result->num_rows > 0) {
+    echo "<script>alert('This email is already in use');</script>";
+} else {
+    if ($reg_password !== $_POST["passWord-2"]) {
+        echo "<script>alert('Passwords do not match');</script>";
+    } else {
+        $sql = "INSERT INTO user (user_first_name, user_last_name, user_mail, user_password, profile_picture) 
+                VALUES ('$reg_firstname', '$reg_lastname', '$reg_email', '$reg_password', '$file_name')";
 
-
-$result = $conn -> query($sql);
-if($conn->query($sql))
-{
-    echo "<script> alert('Insert Successful!')</script>";
-    // header('location:#');//Re-direct to home
-}
-else{
-        echo "Error".$conn->error;
+        if ($conn->query($sql)) {
+            echo "<script>alert('Registration Successful!');</script>";
+        } else {
+            echo "Error: " . $conn->error;
+        }
     }
-    
 }
-
-
-    
-if($result){
-    echo "Insert Successful!";
-}
-
 
 $conn->close();
-
-
-
-
-
-
 
 ?>
